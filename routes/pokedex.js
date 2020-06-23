b769987485606a4584e5b6086a53ba1e8408cef7
Mattia Carchedi <mattia.carchedi@gmail.com>
2020-06-23 11:39:55 +0200

    added cors middleware

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -7,26 +7,26 @@ router.get("/", function (req, res, next) {
 	res.send(pokedex);
 });
 
-router.get('/:id/:info', function(req, res, next) {
-    const { id, info } = req.params;
-    const idAsNumber = parseInt(id, 10);
+router.get("/:id/:info", function (req, res, next) {
+	const { id, info } = req.params;
+	const idAsNumber = parseInt(id, 10);
 
-    const foundPokemon = pokedex.find((singlePokemon) => {
-        return singlePokemon.id === idAsNumber;
-    });
+	const foundPokemon = pokedex.find((singlePokemon) => {
+		return singlePokemon.id === idAsNumber;
+	});
 
-    res.send(foundPokemon[info]);
+	res.send(foundPokemon[info]);
 });
 
-router.get('/:id', function(req, res, next) {
-    const { id } = req.params;
-    const idAsNumber = parseInt(id, 10);
+router.get("/:id", function (req, res, next) {
+	const { id } = req.params;
+	const idAsNumber = parseInt(id, 10);
 
-    const foundPokemon = pokedex.find((singlePokemon) => {
-        return singlePokemon.id === idAsNumber;
-    });
+	const foundPokemon = pokedex.find((singlePokemon) => {
+		return singlePokemon.id === idAsNumber;
+	});
 
-    res.send(foundPokemon);
+	res.send(foundPokemon);
 });
 
 module.exports = router;
